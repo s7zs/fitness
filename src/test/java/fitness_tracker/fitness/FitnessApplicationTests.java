7acd1b51b7848752237fae3a0449eb8d3174f8bc
s7zs <hussinayman766@gmail.com>
2025-08-15 19:50:07 +0300

    v1

---
+++ src/test/java/fitness_tracker/fitness/FitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package fitness_tracker.fitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
