e8558cb94aaeb9b76cc7f30efa79796abcbb7eaa
s7zs <hussinayman766@gmail.com>
2025-08-15 19:55:15 +0300

    Update FitnessApplication.java

--- src/main/java/fitness_tracker/fitness/FitnessApplication.java
+++ src/main/java/fitness_tracker/fitness/FitnessApplication.java
@@ -3,6 +3,8 @@ package fitness_tracker.fitness;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class FitnessApplication {
 
