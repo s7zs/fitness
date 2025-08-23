8c21cc99db346785bc06e3a90c3de82dd7aece7a
zariiee33 <mohamed.zarie2001@gmail.com>
2025-08-24 00:12:51 +0300

    elogin bardo tmaam

--- src/main/java/fitness_tracker/fitness/Repository/ExerciseRepo.java
+++ src/main/java/fitness_tracker/fitness/Repository/ExerciseRepo.java
@@ -2,10 +2,12 @@ package fitness_tracker.fitness.Repository;
 
 import fitness_tracker.fitness.model.exercise;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface ExerciseRepo extends JpaRepository<exercise,Long> {
+    @Query(nativeQuery = true , value = "SELECT * FROM USERS")
     List<exercise> findbyworkplanid (Long workplanid);
 }
--- src/main/java/fitness_tracker/fitness/Repository/NoteRepo.java
+++ src/main/java/fitness_tracker/fitness/Repository/NoteRepo.java
@@ -2,11 +2,14 @@ package fitness_tracker.fitness.Repository;
 
 import fitness_tracker.fitness.model.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface NoteRepo extends JpaRepository<Note,Long> {
-List<Note>findbyuserid (Long userid);
-List<Note>findbycoachid(Long coachid);
+    @Query(nativeQuery = true , value = "SELECT * FROM USERS")
+    List<Note>findbyuserid (Long userid);
+    @Query(nativeQuery = true , value = "SELECT * FROM USERS")
+    List<Note>findbycoachid(Long coachid);
 }
